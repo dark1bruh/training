fa60786603ba414bb62995fee5cea781afcf11c4
dark1bruh <andryushka2704@gmail.com>
2022-08-08 20:58:10 +0200

    Modify any app

--- src/project_test/any.java
+++ src/project_test/any.java
@@ -4,7 +4,7 @@ public class any {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world!");
-		System.out.println("I am adding code to my proj!");
+		System.out.println("I am adding another code to my proj!");
 	}
 
 }
